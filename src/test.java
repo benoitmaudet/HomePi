2b2c013b4f28dd18251465a2fdfb90e700c91f0b
Benoît <benoit.maudet@gmail.com>
2013-09-13 23:40:34 +0200

    Test

---
+++ src/test.java
@@ -0,0 +1,17 @@
+/**
+ * HomePi
+ * Author: Benoît Maudet
+ * Date: 13/09/13
+ * Time: 23:40
+ */
+
+public class test {
+
+    public test() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
