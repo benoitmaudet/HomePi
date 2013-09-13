99139dae65ca0afdebcfd2a4ddf032260c55a2f5
benoitmaudet <benoit.maudet@gmail.com>
2013-09-14 00:08:44 +0200

    Test

---
+++ src/test/PlaygroundTest.java
@@ -0,0 +1,38 @@
+package test;
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* Playground Tester. 
+* 
+* @author Benoît Maudet
+* @version 1.0 
+*/ 
+public class PlaygroundTest { 
+
+@Before
+public void before() throws Exception {
+
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+@Test
+public void FirstMethodTest() throws Exception { 
+//TODO: FirstMethodTest goes here... 
+} 
+
+@Test
+public void SecondMethodTest() throws Exception { 
+//TODO: SecondMethodTest goes here... 
+} 
+
+@Test
+public void MainTest() throws Exception { 
+//TODO: MainTest goes here... 
+} 
+} 
