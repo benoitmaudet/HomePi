3eee5d2a969099bf7a214889f450a1f86f912560
benoitmaudet <benoit.maudet@gmail.com>
2013-09-14 00:07:21 +0200

    Test

---
+++ src/Playground.java
@@ -0,0 +1,25 @@
+/**
+ * HomePi
+ * Author: Benoît Maudet
+ * Date: 13/09/13
+ * Time: 21:52
+ */
+
+public class Playground {
+
+    public Playground() {
+
+    }
+
+    public void firstMethod(){
+
+    }
+
+    public void secondMethod(){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
