3c63aa86404408a1f17f6fca45449ec23b154b50
benoitmaudet <benoit.maudet@gmail.com>
2013-09-21 01:23:01 +0200

    Test

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 url="http://www.java.net/download/jdk8/archive/b108/binaries/jdk-8-ea-b108-linux-arm-vfp-hflt-18_sep_2013.tar.gz"
 file="jdk-8-ea-b108-linux-arm-vfp-hflt-18_sep_2013.tar.gz"
+
 wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com" $url
 tar zxvf $file -C /opt
 update-alternatives --install "/usr/bin/java" "java" "/opt/jdk1.8.0/bin/java" 1
