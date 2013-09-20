95c2da960bbafc436f160039a6c33ebbed57abb3
benoitmaudet <benoit.maudet@gmail.com>
2013-09-21 00:51:40 +0200

    Test

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget --no-cookies --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com" "http://www.java.net/download/jdk8/archive/b108/binaries/jdk-8-ea-b108-linux-arm-vfp-hflt-18_sep_2013.tar.gz"
+wget --no-check-certifcate --no-cookies --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com" "http://www.java.net/download/jdk8/archive/b108/binaries/jdk-8-ea-b108-linux-arm-vfp-hflt-18_sep_2013.tar.gz"
