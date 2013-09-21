38fecf148a26e4b975654fdacf9f6962d2c3f0c1
benoitmaudet <benoit.maudet@gmail.com>
2013-09-21 15:07:31 +0200

    Test

--- applicationUpdate.sh
+++ applicationUpdate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git config core.fileMode false
 git pull
-/opt/jdk1.8.0/bin/javac src/*.java -d ./bin -cp src-backend/ src
+/opt/jdk1.8.0/bin/javac -sourcepath src -d ./bin -cp src-backend/ src/**/*.java
