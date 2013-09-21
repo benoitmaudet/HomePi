f2875d98c93f10a79dc49d85904abac4d6818119
benoitmaudet <benoit.maudet@gmail.com>
2013-09-21 14:13:06 +0200

    Test

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+echo "Lauch raspi-config to expend sdcard partition"
 
+# git config core.fileMode false
 url="http://www.java.net/download/jdk8/archive/b108/binaries/jdk-8-ea-b108-linux-arm-vfp-hflt-18_sep_2013.tar.gz"
 file="jdk-8-ea-b108-linux-arm-vfp-hflt-18_sep_2013.tar.gz"
 
@@ -7,3 +9,5 @@ wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2F
 tar zxvf $file -C /opt
 update-alternatives --install "/usr/bin/java" "java" "/opt/jdk1.8.0/bin/java" 1
 update-alternatives --config javac
+
+rm $file
---
+++ src-backend/general/Activable.java
@@ -0,0 +1,13 @@
+package general;
+
+/**
+ * HomePi
+ * Author: Benoît Maudet
+ * Date: 14/09/13
+ * Time: 15:20
+ */
+public interface Activable {
+    public void activate();
+    public void deactivate();
+
+}
