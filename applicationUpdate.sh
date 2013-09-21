fd8bf98072899da05dde9e69cfbd14f32c03b1e1
benoitmaudet <benoit.maudet@gmail.com>
2013-09-21 16:17:14 +0200

    Test

--- applicationUpdate.sh
+++ applicationUpdate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git config core.fileMode false
 git pull
-find . -name "*.java" | xargs /opt/jdk1.8.0/bin/javac -classpath .:classes:/var/www/HomePi/lib/'*' -d bin/
+find . -name "*.java" | xargs /opt/jdk1.8.0/bin/javac -classpath .:classes:/var/www/HomePi/lib/'*' -d /var/www/HomePi/bin/
 #To run : sudo java -classpath .:classes:/var/www/HomePi/lib/'*' Playground
