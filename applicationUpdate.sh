f313782c2ccfd9906312d4a5ad139fed43c69b31
benoitmaudet <benoit.maudet@gmail.com>
2013-09-21 14:59:03 +0200

    Test

--- applicationUpdate.sh
+++ applicationUpdate.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-if [ -f install.sh ]
-then
- rm install.sh
-fi
 git pull
-chmod +x install.sh
-/opt/jdk1.8.0/bin/javac src/*.java -d ./bin -cp src-backend/
+/opt/jdk1.8.0/bin/javac src/*.java -d ./bin -cp src-backend/ src
