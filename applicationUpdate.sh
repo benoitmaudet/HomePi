7ed7d635f753917a3aba4758e3bb77fdd9389d13
benoitmaudet <benoit.maudet@gmail.com>
2013-09-21 15:18:02 +0200

    Test

--- applicationUpdate.sh
+++ applicationUpdate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git config core.fileMode false
 git pull
-/opt/jdk1.8.0/bin/javac -sourcepath src -d ./bin -cp src-backend/ src/**/*.java
+/opt/jdk1.8.0/bin/javac -sourcepath src -d ./bin -cp src-backend/ -cp lib/junit-4.11.jar src/**/*.java
