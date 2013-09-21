bf2ce2a42091e4f70fd2fda9827b9dc9c002a369
benoitmaudet <benoit.maudet@gmail.com>
2013-09-21 16:06:39 +0200

    Test

--- applicationUpdate.sh
+++ applicationUpdate.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 git config core.fileMode false
 git pull
-/opt/jdk1.8.0/bin/javac -sourcepath src -d ./bin -cp src-backend/ -cp lib/junit-4.11.jar src/**/*.java
+find . -name "*.java" | xargs /opt/jdk1.8.0/bin/javac -classpath .:classes:/home/pi/HomePi/lib/'*' -d bin/
+#To run : sudo java -classpath .:classes:/opt/pi4j/lib/'*' Playground
--- src/Playground.java
+++ src/Playground.java
@@ -26,32 +26,42 @@ public class Playground {
         final GpioController gpio = GpioFactory.getInstance();
 
         // provision gpio pin #04 as an output pin and turn on
-        final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, "MyLED", PinState.HIGH);
+        final GpioPinDigitalOutput pin3 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_03, "MyLED", PinState.HIGH);
+        final GpioPinDigitalOutput pin4 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, "MyLED", PinState.HIGH);
+        final GpioPinDigitalOutput pin5 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_05, "MyLED", PinState.HIGH);
         System.out.println("--> raspberrypi.GPIO state should be: ON");
 
         Thread.sleep(15000);
 
         // turn off gpio pin #04
-        pin.low();
+        pin3.low();
+        pin4.low();
+        pin5.low();
         System.out.println("--> raspberrypi.GPIO state should be: OFF");
 
         Thread.sleep(15000);
 
         // toggle the current state of gpio pin #04 (should turn on)
-        pin.toggle();
+        pin5.toggle();
+        pin4.toggle();
+        pin3.toggle();
         System.out.println("--> raspberrypi.GPIO state should be: ON");
 
         Thread.sleep(15000);
 
         // toggle the current state of gpio pin #04  (should turn off)
-        pin.toggle();
+        pin5.toggle();
+        pin4.toggle();
+        pin3.toggle();
         System.out.println("--> raspberrypi.GPIO state should be: OFF");
 
         Thread.sleep(15000);
 
         // turn on gpio pin #04 for 1 second and then off
         System.out.println("--> raspberrypi.GPIO state should be: ON for only 1 second");
-        pin.pulse(1000, true); // set second argument to 'true' use a blocking call
+        pin3.pulse(1000, true); // set second argument to 'true' use a blocking call
+        pin4.pulse(1000, true); // set second argument to 'true' use a blocking call
+        pin5.pulse(1000, true); // set second argument to 'true' use a blocking call
 
         // stop all raspberrypi.GPIO activity/threads by shutting down the raspberrypi.GPIO controller
         // (this method will forcefully shutdown all raspberrypi.GPIO monitoring threads and scheduled tasks)
