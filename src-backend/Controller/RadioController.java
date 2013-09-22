417f1a540ae260fda5752b5417a1f7a1c08a07cd
benoitmaudet <benoit.maudet@gmail.com>
2013-09-22 13:44:24 +0200

    Test

--- src-backend/Controller/RadioController.java
+++ src-backend/Controller/RadioController.java
@@ -1,5 +1,9 @@
 package controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * HomePi
  * Author: Benoît Maudet
@@ -18,8 +22,17 @@ public class RadioController extends Controller{
         return null;  //To change body of implemented methods use File | Settings | File Templates.
     }
 
-    public void sendMessage(){
+    public static void sendMessage(String message) throws IOException, InterruptedException {
+        Process p = Runtime.getRuntime().exec("sudo rf_utils/send " + message);
+        p.waitFor();
 
+        BufferedReader reader =
+                new BufferedReader(new InputStreamReader(
+                        p.getInputStream()));
+        String line = reader.readLine();
+        while (line != null) {
+            line = reader.readLine();
+        }
     }
 
     public static void main(String[] args) {
